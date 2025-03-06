265d54811a1b35a4f5b1ef02388f26c12b878e7a
TainSiddik <tainsiddik11@gmail.com>
2025-03-06 11:47:47 +0700

    add verifyToken

--- routers/routerKategori.js
+++ routers/routerKategori.js
@@ -1,11 +1,12 @@
 import express from "express"
 import { getAllKategori, addKategori, editKategori, hapusKategori } from "../controllers/controllerKategori.js"
+import { verifyToken } from "../middleware/verifyToken.js"
 
 const router = express.Router()
 
 router.get('/kategori', getAllKategori)
-router.post('/kategori', addKategori)
-router.patch('/kategori/:id', editKategori)
-router.delete('/kategori/:id', hapusKategori)
+router.post('/kategori', verifyToken, addKategori)
+router.patch('/kategori/:id', verifyToken, editKategori)
+router.delete('/kategori/:id', verifyToken, hapusKategori)
 
 export default router
